229410a5900966458fde7c53868d2979a3a04314
haruto0214 <135305007+haruto0214@users.noreply.github.com>
2023-06-13 17:54:34 +0900

    #2

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 
+import math
 import streamlit as st
-import sqlite3
+import pandas as pd
 
-import streamlit as st
+st.text("ヤッハロー")
+#         ↑ガハマすこ
 
 # 投稿データを保持するリスト
 posts = []
@@ -19,48 +21,23 @@ def show_posts():
         st.write('---')
 
 # 掲示板アプリのタイトル
-st.title("Bulletin Board App")
+st.title("舞チャン")
 
 # 新しい投稿の作成
-st.header("Create New Post")
-title = st.text_input("Title")
-content = st.text_area("Content")
-if st.button("Create"):
+st.header("新しい投稿を作成")
+title = st.text_input("スレタイトル")
+content = st.text_area("内容")
+if st.button("作成！"):
     create_post(title, content)
-    st.success("Post created successfully!")
-
-# 投稿一覧の表示
-st.header("Posts")
-if len(posts) == 0:
-    st.info("No posts yet.")
-else:
-    show_posts()
-
-# データベース接続とテーブル作成
-conn = sqlite3.connect('posts.db')
-c = conn.cursor()
-c.execute('''CREATE TABLE IF NOT EXISTS posts (content TEXT)''')
-
+    st.success("作成完了！")
 # 投稿の保存
 def save_post(content):
     c.execute("INSERT INTO posts VALUES (?)", (content,))
     conn.commit()
-
-# Streamlitアプリケーションの設定
-st.title('掲示板アプリ')
-
-# 投稿フォーム
-new_post = st.text_input('新規投稿')
-if st.button('投稿'):
-    save_post(new_post)
-    st.success('投稿が保存されました。')
-
-# データベースから投稿を読み込み表示
-c.execute("SELECT * FROM posts")
-posts = c.fetchall()
-for post in posts:
-    st.write(f'- {post[0]}')
-
-# データベース接続をクローズ
-conn.close()
+# 投稿一覧の表示
+st.header("スレタイトル一覧")
+if len(posts) == 0:
+    st.info("まだ投稿はありません")
+else:
+    show_posts()
 
