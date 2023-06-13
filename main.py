1f7fe387894e5b6aa0ebcf8c403b4b5060ae0593
haruto0214 <135305007+haruto0214@users.noreply.github.com>
2023-06-13 18:16:49 +0900

    ＃４

--- main.py
+++ main.py
@@ -38,23 +38,37 @@ if len(posts) == 0:
 else:
     show_posts()
 
+import streamlit as st
 
-# 投稿の保存
-def save_post(content):
-    posts.append(content)
+# 禁止ワードのリスト
+banned_words = ["禁止ワード1", "禁止ワード2", "禁止ワード3"]
 
-# Streamlitアプリケーションの設定
-st.title('掲示板アプリ')
+# ユーザーの投稿内容をチェックする関数
+def check_post_content(post_content):
+    # 禁止ワードの検出
+    for banned_word in banned_words:
+        if banned_word in post_content:
+            return True # 禁止ワードが検出された場合はTrueを返す
+    return False # 禁止ワードが検出されなかった場合はFalseを返す
 
-# 投稿フォーム
-new_post = st.text_input('新規投稿')
-if st.button('投稿'):
-    save_post(new_post)
-    st.success('投稿が保存されました。')
+# 掲示板アプリのメイン処理
+def main():
+    st.title("掲示板アプリ")
+    
+    # ユーザーの投稿内容を入力
+    post_content = st.text_input("投稿内容")
+    
+    # 投稿ボタンがクリックされた場合の処理
+    if st.button("投稿"):
+        # 禁止ワードのチェック
+        if check_post_content(post_content):
+            # 禁止ワードが検出された場合は警報を出す
+            st.warning("禁止ワードが含まれています！")
+        
+        # 投稿内容を表示
+        st.write("投稿内容:", post_content)
 
-# リスト内の投稿を表示
-if len(posts) > 0:
-    df = pd.DataFrame(posts, columns=['投稿内容'])
-    st.dataframe(df)
-else:
-    st.warning('まだ投稿がありません。')
+# アプリの実行
+if __name__ == "__main__":
+    main()
+
