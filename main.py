ac30ad7961de59fbf1046497f0827851601fe80c
haruto0214 <135305007+haruto0214@users.noreply.github.com>
2023-06-23 15:02:40 +0900

    ＃１２

--- main.py
+++ main.py
@@ -1,78 +1,47 @@
 import streamlit as st
 import json
+from datetime import datetime
+import pytz
 
 # 禁止ワードのリスト
 banned_words = ["馬鹿", "禁止ワード2", "禁止ワード3"]
-
-# ユーザーの投稿内容をチェックする関数
-def check_post_content(title, content):
-    # タイトルと投稿内容の禁止ワードの検出
-    for banned_word in banned_words:
-        if banned_word in title:
-            title = title.replace(banned_word, "＠" * len(banned_word))
-        if banned_word in content:
-            content = content.replace(banned_word, "＠" * len(banned_word))
+@@ -16,7 +17,7 @@ def check_post_content(title, content):
     return title, content
 
 def save_post(title, content):
-    post = {"title": title, "content": content}
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    now = datetime.now(pytz.timezone("Asia/Tokyo")).strftime("%Y-%m-%d %H:%M:%S")
+    post = {"title": title, "content": content, "timestamp": now}
     with open('posts.json', 'a') as file:
-        json.dump(post, file)
+        file.write(json.dumps(post))
         file.write('\n')
-
 def load_posts():
     with open('posts.json', 'r') as file:
-        return [json.loads(line) for line in file]
-
+        lines = file.readlines()
+        return [json.loads(line) for line in lines]
 def main():
     st.title("掲示板アプリ")
-
     # 新規投稿の入力
     new_post_title = st.text_input("タイトル")
     new_post_content = st.text_area("新規投稿", height=100)
-
     # 投稿ボタンが押された場合
     if st.button("投稿する") and new_post_title and new_post_content:
         new_post_title, new_post_content = check_post_content(new_post_title, new_post_content)
         if "＠" in new_post_title or "＠" in new_post_content:
             st.warning("禁止ワードが含まれています！")
-
         save_post(new_post_title, new_post_content)
         st.success("投稿が保存されました！")
-
     # 保存された投稿の表示
     posts = load_posts()
     st.subheader("保存された投稿")
-
     if not posts:
         st.info("まだ投稿がありません。")
     else:
         for post in posts:
-            # 各タイトルにリンクを付けて表示
-           import streamlit as st
-
-posts = [
-    {
-        'title': 'タイトル1',
-        'content': 'タイトル1のコンテンツ'
-    },
-    {
-        'title': 'タイトル2',
-        'content': 'タイトル2のコンテンツ'
-    },
-    {
-        'title': 'タイトル3',
-        'content': 'タイトル3のコンテンツ'
-    }
-]
-
-for post in posts:
-    post_url = f"[{post['title']}](#{post['title']})"
-    st.markdown(post_url, unsafe_allow_html=True)
-    st.write(post['content'])
-    st.markdown("---")
-
+            st.text(post["title"])
+            st.text(post["content"])
+            st.text("投稿時刻: " + post.get("timestamp", ""))
+            st.markdown("---")
 if __name__ == "__main__":
     main()
-
 
