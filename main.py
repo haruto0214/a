572f491a82cd2eb5ea8f959c942d3da62724e2a6
haruto0214 <135305007+haruto0214@users.noreply.github.com>
2023-07-06 17:48:46 +0900

    ＃２２

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ def main():
     if len(messages) > 0:
         st.subheader("メッセージの削除")
         message_to_delete = st.selectbox("あ:", messages)
-        delete_word = st.text_input("削除ワードを入力してください:")
+        delete_word = st.text_input("あ:")
         if st.button("削除") and delete_word in message_to_delete:
             delete_message(message_to_delete)
             st.success("メッセージが削除されました。")
