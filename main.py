a752f1a2d2ff6134e34bbd3675445cb3637b738b
haruto0214 <135305007+haruto0214@users.noreply.github.com>
2023-07-06 17:06:57 +0900

    #21

--- main.py
+++ main.py
@@ -8,18 +8,17 @@ import urllib.parse
 banned_words = ["馬鹿", "禁止ワード2", "禁止ワード3"]
 
 # ユーザーの投稿内容をチェックする関数
-def check_post_content(title, content):
+def check_post_content(content):
     # タイトルと投稿内容の禁止ワードの検出
     for banned_word in banned_words:
-        if banned_word in title:
-            title = title.replace(banned_word, "＠" * len(banned_word))
         if banned_word in content:
             content = content.replace(banned_word, "＠" * len(banned_word))
-    return title, content
+    return content
 
-def save_post(title, content):
-    now = datetime.now(pytz.timezone("Asia/Tokyo")).strftime("%Y-%m-%d %H:%M:%S")
-    post = {"title": title, "content": content, "timestamp": now}
+def save_post(content):
+    now = datetime.now(pytz.timezone("Asia/Tokyo"))
+    now_str = now.strftime("%Y-%m-%d %H:%M:%S")
+    post = {"content": content, "timestamp": now_str}
     with open('posts.json', 'a') as file:
         file.write(json.dumps(post))
         file.write('\n')
@@ -27,22 +26,30 @@ def save_post(title, content):
 def load_posts():
     with open('posts.json', 'r') as file:
         lines = file.readlines()
-        return [json.loads(line) for line in lines]
+        posts = [json.loads(line.strip()) for line in lines]
+        
+        # タイムスタンプを日本時間に変換
+        for post in posts:
+            timestamp = datetime.strptime(post['timestamp'], "%Y-%m-%d %H:%M:%S")
+            timestamp = pytz.timezone("Asia/Tokyo").localize(timestamp)
+            post['timestamp'] = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+
+        return posts
 
 def main():
-    st.title("掲示板アプリ")
+    st.title("テスト")
 
     # 新規投稿の入力
+    new_post_content = st.text_area("投稿", height=100)
+
     
-    new_post_content = st.text_area("新規投稿", height=100)
-    new_post_title = st.text_input("ページ")
     # 投稿ボタンが押された場合
-    if st.button("投稿する") and new_post_title and new_post_content:
-        new_post_title, new_post_content = check_post_content(new_post_title, new_post_content)
-        if "＠" in new_post_title or "＠" in new_post_content:
+    if st.button("投稿する") and new_post_content:
+        new_post_content = check_post_content(new_post_content)
+        if "＠" in new_post_content:
             st.warning("禁止ワードが含まれています！")
 
-        save_post(new_post_title, new_post_content)
+        save_post(new_post_content)
         st.success("投稿が保存されました！")
 
     # 保存された投稿の表示
@@ -53,11 +60,8 @@ def main():
         st.info("まだ投稿がありません。")
     else:
         for post in posts:
-            # 各タイトルにリンクを付けて表示
-            post_url = f"<a href='https://maichan-bord-{urllib.parse.quote(post['title'])}.streamlit.app'>{post['title']}</a>"
-            st.write(post['content'])
-            st.markdown(post_url, unsafe_allow_html=True)
-            st.text("投稿時刻: " + post.get("timestamp", ""))
+            st.subheader(post['content'])
+            st.write(post['timestamp'])  # タイムスタンプを表示
             st.markdown("---")
 
 if __name__ == "__main__":
