7319d86e6a60c49f43005c72eba09f7665ee4148
haruto0214 <135305007+haruto0214@users.noreply.github.com>
2023-09-22 14:11:50 +0900

    ＃３０

--- main.py
+++ main.py
@@ -20,13 +20,19 @@ def check_post_content(title, content):
 # 投稿ごとのGoodとBadのカウンターを管理する辞書
 post_votes = {}
 
-def save_post(title, content):
+# イラストのGoodとBadのカウンターを管理する辞書
+illustration_votes = {}
+
+def save_post(title, content, is_illustration):
     now = datetime.now(pytz.timezone("Asia/Tokyo"))
     now_str = now.strftime("%Y-%m-%d %H:%M:%S")
     post = {"title": title, "content": content, "timestamp": now_str}
     with open('posts.json', 'a') as file:
         file.write(json.dumps(post))
         file.write('\n')
+    if is_illustration:
+        # イラストの場合、カウンターを初期化
+        illustration_votes[title] = {'good': 0, 'bad': 0}
 
 def load_posts():
     with open('posts.json', 'r') as file:
@@ -48,12 +54,15 @@ def main():
     new_post_content = st.text_area("管理者以外記述厳禁", height=100)
     new_post_title = st.text_input("ページ")
 
+    # イラストかどうかの選択
+    is_illustration = st.checkbox("イラスト")
+
     # 投稿ボタンが押された場合
     if st.button("投稿する") and new_post_title and new_post_content:
         new_post_title, new_post_content = check_post_content(new_post_title, new_post_content)
         if "＠" in new_post_title or "＠" in new_post_content:
             st.warning("禁止ワードが含まれています！")
-        save_post(new_post_title, new_post_content)
+        save_post(new_post_title, new_post_content, is_illustration)
         st.success("投稿が保存されました！")
 
     # 保存された投稿の表示
@@ -88,8 +97,12 @@ def main():
 
             st.subheader(post['content'])
             st.write(post['timestamp'])  # タイムスタンプを表示
+            if is_illustration:
+                st.write(f"Good: {illustration_votes.get(post['title'], {'good': 0}).get('good', 0)}")
+                st.write(f"Bad: {illustration_votes.get(post['title'], {'bad': 0}).get('bad', 0)}")
             st.markdown(post_url, unsafe_allow_html=True)
             st.markdown("---")
 
 if __name__ == "__main__":
     main()
+
